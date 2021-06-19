b1ab0f0da305eb002408821b13e7b2f1ae4f9cc1
Tuyen Giang <nguyentuyengiangbn@gmail.com>
2021-06-19 22:41:12 +0700

    Giang Commit Part 2

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,18 +1,29 @@
 jQuery(document).ready(function ($) {
-
     // Tab
-    function activeTab(obj)
-    {
+    function activeTab(obj) {
         $('.tab-wrapper ul li').removeClass('active');
         $(obj).addClass('active');
         var id = $(obj).find('a').attr('href');
         $('.tab-item').hide();
-        $(id) .show();
+        $(id).show();
     }
 
-    $('.tab-wrapper ul li').click(function(){
+    $('.tab-wrapper ul li').click(function () {
         activeTab(this);
         return false;
     });
     activeTab($('.tab-wrapper ul li:first-child'));
+
+    //active gallery product
+    $('#thumb_img img:first-child').addClass('active');
 });
+
+function changeimg(url, e) {
+    document.getElementById("img").src = url;
+    let nodes = document.getElementById("thumb_img");
+    let img_child = nodes.children;
+    for (i = 0; i < img_child.length; i++) {
+        img_child[i].classList.remove('active')
+    }
+    e.classList.add('active');
+}
